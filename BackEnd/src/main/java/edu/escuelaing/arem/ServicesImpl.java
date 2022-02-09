ef4e42aa8bef75d37834c3da86885cf548d1579f
Laura Garcia <laura.garcia-l@outlook.com>
2022-02-08 22:28:13 -0500

    Update Backend

--- BackEnd/src/main/java/edu/escuelaing/arem/ServicesImpl.java
+++ BackEnd/src/main/java/edu/escuelaing/arem/ServicesImpl.java
@@ -8,6 +8,11 @@ public class ServicesImpl {
         this.services = new Services();
     }
 
+    /**
+     * Utiliza la función de la clase Services para obtener el valor y devolverlo en formato Json
+     * @param celsius, valor a convertir
+     * @return Json con la respuesta y el estado
+     */
     public JsonObject celsiusToFahrenheit(double celsius){
         double ans = services.celsiusToFahrenheit(celsius);
 
@@ -19,6 +24,11 @@ public class ServicesImpl {
         return json;
     }
 
+    /**
+     * Utiliza la función de la clase Services para obtener el valor y devolverlo en formato Json
+     * @param fahrenheit, valor a convertir
+     * @return Json con la respuesta y el estado
+     */
     public JsonObject fahrenheitToCelsius(double fahrenheit){
         double ans = services.fahrenheitToCelsius(fahrenheit);
 
